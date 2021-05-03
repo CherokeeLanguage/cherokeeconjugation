97e7281f112060627eb46d37cc812d0102a90e49
standtrooper <standtrooper@gmail.com>
2021-05-03 17:08:02 -0500

    fixing verb factory for verb generation

--- src/main/groovy/net/cherokeedictionary/factory/VerbFactory.groovy
+++ src/main/groovy/net/cherokeedictionary/factory/VerbFactory.groovy
@@ -3,11 +3,15 @@ package net.cherokeedictionary.factory
 import net.cherokeedictionary.core.Verb
 import net.cherokeedictionary.stemmer.DefinitionLine
 import net.cherokeedictionary.stemmer.Stemmer
+import net.cherokeedictionary.util.PartOfSpeech
+import net.cherokeedictionary.util.PrefixTableObject
+import net.cherokeedictionary.util.PrefixTableSubject
+import net.cherokeedictionary.util.Tense
 import net.cherokeedictionary.verb.conjugation.Conjugate
 
 class VerbFactory {
     //todo: this isn't really a verb factory - it's more of a conjugateAndGetDisplayValue kind of thing
-    static String createVerbFromParameters(paramMap) {
+    static Verb createVerbFromParameters(paramMap) {
         def habitual = paramMap.habitual
         def imperative = paramMap.imperative
         def infinitive = paramMap.infinitive
@@ -16,8 +20,6 @@ class VerbFactory {
         def remotepast = paramMap.remotepast
         def partofspeechc = paramMap.partofspeechc
 
-//        println paramMap
-
         def stemmer = new Stemmer()
         stemmer.habitual = new DefinitionLine(syllabary: habitual)//"ᎦᏬᏂᏍᎪᎢ")
         stemmer.imperative = new DefinitionLine(syllabary: imperative)//"ᎯᏬᏂᎯ")
@@ -25,43 +27,39 @@ class VerbFactory {
         stemmer.present1st = new DefinitionLine(syllabary:  present1st)//"ᏥᏬᏂᎭ")
         stemmer.present3rd = new DefinitionLine(syllabary: present3rd)//"ᎦᏬᏂᎭ")
         stemmer.remotepast = new DefinitionLine(syllabary: remotepast)//"ᎤᏬᏂᏒᎢ")
-//        stemmer.setHabitual(new DefinitionLine(syllabary: habitual))
-//        stemmer.setImperative(new DefinitionLine(syllabary: imperative))
-//        stemmer.setInfinitive(new DefinitionLine(syllabary: infinitive))
-//        stemmer.setPresent1st(new DefinitionLine(syllabary:  present1st))
-//        stemmer.setPresent3rd(new DefinitionLine(syllabary: present3rd))
-//        stemmer.setRemotepast(new DefinitionLine(syllabary: remotepast))
 
-//        Verb word = new Verb()
-//        println partofspeechc
-//        word.partOfSpeech = partofspeechc == 'vi' ? PartOfSpeech.VERB_INTRANSITIVE : PartOfSpeech.VERB_TRANSITIVE
-//
-//        word.tense = Tense.valueOf(paramMap.verbTense ?: "PRESENT")
+        Verb verb = new Verb()
+        verb.initialPrefixHolder.yi = paramMap.yi
+        verb.initialPrefixHolder.ji = paramMap.ji
+        verb.initialPrefixHolder.wi = paramMap.wi
+        verb.initialPrefixHolder.ni = paramMap.ni
+        verb.initialPrefixHolder.de = paramMap.de
+        verb.initialPrefixHolder.da = paramMap.da
+        verb.initialPrefixHolder.di = paramMap.dis
+        verb.initialPrefixHolder.i = paramMap.i
+        verb.initialPrefixHolder.ga = paramMap.ga
+        verb.initialPrefixHolder.e = paramMap.e
 
-        //rewrite so this works with the latest version
-//        word.prefixHolderObject.with {
-//            yi = params.yi == 'on'
-//            ji = params.ji == 'on'
-//            wi = params.wi == 'on'
-//            ni = params.ni == 'on'
-//            de = params.de == 'on'
-//            da = params.da == 'on'
-//            di = params.di == 'on'
-//            i = params.i == 'on'
-//            ga = params.ga == 'on'
-//            e = params.e == 'on'
-//        }
-//
-        def displayValue
-        try {
-            Verb verb = Conjugate.conjugate(paramMap.subject, paramMap.object, stemmer, paramMap.verbTense, partofspeechc, false)
-//            println verb
-            displayValue = verb.wholeWord
-        } catch (Exception e) {
-            displayValue = "there was an error with your request"
+        if (partofspeechc == 'v.i.' || partofspeechc == 'vi') {
+            partofspeechc = PartOfSpeech.VERB_INTRANSITIVE
+        } else {
+            partofspeechc = PartOfSpeech.VERB_TRANSITIVE
         }
 
+        verb.subject = paramMap.subject ?:PrefixTableSubject.valueOf(paramMap.subject)
+        verb.object = paramMap.object ?: PrefixTableObject.valueOf(paramMap.object)
 
-        return displayValue
+        verb.partOfSpeech = partofspeechc
+        verb.stemmer = stemmer
+        verb.tense = Tense.valueOf(paramMap.verbTense)
+
+        return verb
+    }
+
+    static Verb createVerbFromParametersAndConjugate(paramMap) {
+        Verb verb = createVerbFromParameters(paramMap)
+        verb = Conjugate.conjugate(verb)
+
+        return verb
     }
 }
--- src/test/groovy/net/cherokeedictionary/factory/VerbFactoryTest.groovy
+++ src/test/groovy/net/cherokeedictionary/factory/VerbFactoryTest.groovy
@@ -1,11 +1,60 @@
 package net.cherokeedictionary.factory
 
 import net.cherokeedictionary.conjugation.ConjugateBase
+import net.cherokeedictionary.core.Verb
+import net.cherokeedictionary.verb.conjugation.Conjugate
 
 class VerbFactoryTest extends ConjugateBase {
     void testVerbFactory() {
-        def paramMap = [habitual:'ᎠᏔᏍᎩᏍᎪᎢ', imperative:'ᎭᏔᏍᎩ', infinitive:'ᎤᏔᏍᎩᏍᏗ', present1st:'ᎦᏔᏍᎩᎠ', present3rd:'ᎠᏔᏍᎩᎠ', remotepast:'ᎤᏔᏍᎩᏒᎢ', partofspeechc:'vi', verbTense:'PRESENT', subject:'SG1', object:null]
-        def display = VerbFactory.createVerbFromParameters(paramMap)
+        def paramMap = [:]
+        paramMap.habitual = 'ᎠᏔᏍᎩᏍᎪᎢ'
+        paramMap.imperative = 'ᎭᏔᏍᎩ'
+        paramMap.infinitive = 'ᎤᏔᏍᎩᏍᏗ'
+        paramMap.present1st = 'ᎦᏔᏍᎩᎠ'
+        paramMap.present3rd = 'ᎠᏔᏍᎩᎠ'
+        paramMap.remotepast = 'ᎤᏔᏍᎩᏒᎢ'
+        paramMap.partofspeechc = 'vi'
+        paramMap.verbTense = 'PRESENT'
+        paramMap.subject = "SG1"
+        paramMap.object = "SG3AN"
+//        paramMap.yi = params.yi == 'on'
+//        paramMap.ji = false
+//        paramMap.wi = false
+//        paramMap.ni = params.ni == 'on'
+//        paramMap.de = params.de == 'on'
+//        paramMap.da = params.da == 'on'
+//        paramMap.di = params.di == 'on'
+//        paramMap.i = params.i == 'on'
+//        paramMap.ga = params.ga == 'on'
+//        paramMap.e = params.e == 'on'
+        Verb display = VerbFactory.createVerbFromParameters(paramMap)
+        println display
+    }
+
+    void testVerbFactoryYiGowatiha() {
+        def paramMap = [:]
+        paramMap.habitual = 'ᎠᏔᏍᎩᏍᎪᎢ'
+        paramMap.imperative = 'ᎭᏔᏍᎩ'
+        paramMap.infinitive = 'ᎤᏔᏍᎩᏍᏗ'
+        paramMap.present1st = 'ᎦᏔᏍᎩᎠ'
+        paramMap.present3rd = 'ᎠᏔᏍᎩᎠ'
+        paramMap.remotepast = 'ᎤᏔᏍᎩᏒᎢ'
+        paramMap.partofspeechc = 'vi'
+        paramMap.verbTense = 'PRESENT'
+        paramMap.subject = "SG1"
+        paramMap.object = "SG3AN"
+        paramMap.yi = true
+//        paramMap.ji = false
+//        paramMap.wi = false
+//        paramMap.ni = params.ni == 'on'
+//        paramMap.de = params.de == 'on'
+//        paramMap.da = params.da == 'on'
+//        paramMap.di = params.di == 'on'
+//        paramMap.i = params.i == 'on'
+//        paramMap.ga = params.ga == 'on'
+//        paramMap.e = params.e == 'on'
+        Verb display = VerbFactory.createVerbFromParameters(paramMap)
+        display = Conjugate.conjugate(display)
         println display
     }
 }
